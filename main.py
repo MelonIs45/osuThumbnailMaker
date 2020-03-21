8d5645c4e32cf0df63993a00b03ba7b37a7830ef
MelonIs45 <45884578+MelonIs45@users.noreply.github.com>
2020-03-21 18:53:22 +0000

    Add files via upload
    added image finder for beatmaps and opens them

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import PIL, json, os, glob, re
-
+from PIL import Image
 user = os.getlogin()
 osuDir = os.path.join('C:\\Users', user,'AppData/Local/osu!/Songs')
-fileExt = ['png', 'jpg']
+fileExt = ('png', 'jpg')
 
 songString = input("Enter the beatmap id of the song you wish to be made a thumbnail for: ")
-#songString = "402887"
 
 for songDir in glob.glob(osuDir + '/*'):
     if songString in songDir:
         print(songDir)
-        print(os.listdir(songDir))
+        songDirFiles = os.listdir(songDir)
+        print(songDirFiles)
+        for i in range(len(songDirFiles)):
+            if songDirFiles[i - 1].endswith(fileExt):
+                bgDir = songDirFiles[i - 1]
+                bgDirFull = os.path.join(songDir, bgDir)
+            else:
+                pass
     else:
         pass
 
-
-
+bg = Image.open(bgDirFull)
+bg.show()
 input()
