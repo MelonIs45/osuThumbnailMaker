32e14e03a8d4a41befb7a5a5c848295d565e8314
MelonIs45 <45884578+MelonIs45@users.noreply.github.com>
2020-03-21 21:00:16 +0000

    added file checking etc + more options

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 import PIL, json, os, glob, re
 from PIL import Image
 user = os.getlogin()
-osuDir = os.path.join('C:\\Users', user,'AppData/Local/osu!/Songs')
-fileExt = ('png', 'jpg')
+osuDir = os.path.join('C:/Users', user,'AppData/Local/osu!/Songs')
+fileExts = ('png', 'jpg')
+trueRanks = ('XH', 'X', 'SH', 'S', 'A', 'B', 'C', 'D', 'F')
 
-songString = input("Enter the beatmap id of the song you wish to be made a thumbnail for: ")
+#question booleans
+idQuestion = False
+oneMap = False
+rankQuestion = False
 
+while idQuestion == False:
+    try:
+        songId = int(input("Enter the beatmap id of the song you wish to be made a thumbnail for: "))
+        idQuestion = True
+    except:
+        print("You didn't enter a correct value.")
+
+
+#gets the path to the image in the beatmap folder
 for songDir in glob.glob(osuDir + '/*'):
-    if songString in songDir:
-        print(songDir)
-        songDirFiles = os.listdir(songDir)
-        print(songDirFiles)
-        for i in range(len(songDirFiles)):
-            if songDirFiles[i - 1].endswith(fileExt):
-                bgDir = songDirFiles[i - 1]
-                bgDirFull = os.path.join(songDir, bgDir)
-            else:
-                pass
+    if str(songId) in songDir:
+        while oneMap == False:
+            print(songDir)
+            songDirFiles = os.listdir(songDir)
+            print(songDirFiles)
+            oneMap = True
+            for i in range(len(songDirFiles)):
+                if songDirFiles[i - 1].endswith(fileExts):
+                    bgName = songDirFiles[i - 1]
+                    bgDirFull = os.path.join(songDir, bgName)
+                else:
+                    pass
     else:
         pass
+
+while rankQuestion == False:
+    playRank = input("Enter the rank of the play (XH, X, SH, S, A, B, C, D, F): ")
+    if playRank in trueRanks:
+        rankQuestion = True
+    else:
+        print("You entered an incorrect rank.")
+        
+userName = input("Enter the user that for that play: ")
+modsUsed = input("Enter string of mods used (HDDT, HDHR, etc.): ")
+ppCount = int(input("Enter the pp from that play: "))
+accCount = float(input("Enter the play accuracy: "))
+starCount = float(input("Enter the play star count: "))
 
 bg = Image.open(bgDirFull)
 bg.show()
