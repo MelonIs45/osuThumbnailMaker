0586c4c9dd9c1dd2e5ebd7cd81090e6464fe70c6
MelonIs45 <45884578+MelonIs45@users.noreply.github.com>
2020-03-23 12:50:26 +0000

    Add files via upload
    added more comments

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ user = os.getlogin()
 
 osuDir = os.path.join('C:/Users', user,'AppData/Local/osu!/Songs')
 fileExts = ('png', 'jpg')
-trueRanks = ('XH', 'X', 'SH', 'S', 'A', 'B', 'C', 'D', 'F')
+# trueRanks = ('XH', 'X', 'SH', 'S', 'A', 'B', 'C', 'D', 'F')
 imgSize = (1280, 720)
 fontSizeUserName = 32
 fontSizeTitle = 21
@@ -24,6 +24,7 @@ oneMap = None
 rankQuestion = None
 colourGrabbing = None
 
+#uses the id to search for the song background
 while idQuestion is None:
     try:
         songId = int(input("Enter the beatmap id of the song you wish to be made a thumbnail for: "))
@@ -45,18 +46,16 @@ for songDir in glob.glob(osuDir + '/*'):
                     bgDirFull = os.path.join(songDir, bgName)
                 else:
                     pass
-            
     else:
         pass
 
-
 #asks for rank of play
-while rankQuestion is None:
-    playRank = input("Enter the rank of the play (XH, X, SH, S, A, B, C, D, F): ")
-    if playRank in trueRanks:
-        rankQuestion = True
-    else:
-        print("You entered an incorrect rank.")
+# while rankQuestion is None:
+#     playRank = input("Enter the rank of the play (XH, X, SH, S, A, B, C, D, F): ")
+#     if playRank in trueRanks:
+#         rankQuestion = True
+#     else:
+#         print("You entered an incorrect rank.")
         
 #extra details for thumbnail
 userName = input("Enter the user that for that play: ")
@@ -66,6 +65,7 @@ ppCount = int(input("Enter the pp from that play: "))
 accCount = float(input("Enter the play accuracy: "))
 starCount = float(input("Enter the play star count: "))
 
+#creates strings
 userNameLength = (len(userName)) * fontSizeUserName
 userNameOffset = userNameLength/2
 userNameBlockOffset = userNameOffset + 50
@@ -90,7 +90,7 @@ print(userNameLength, titleLength, stats)
 bg = Image.open(bgDirFull)
 bg = bg.filter(ImageFilter.GaussianBlur(15))
 bg = bg.resize(imgSize)
-bg.show()
+#bg.show()
 
 
 #finds the most common colours in the image by splitting them up into sectors
@@ -125,6 +125,8 @@ x, y = textBlocks.size
 draw = ImageDraw.Draw(textBlocks)
 
 #draws the blocks to where the text will be on
+
+#1st 3 blocks are the glowy/blurred backgrounds, NOTE: had to use 3 shapes to create a curved square
 draw.ellipse(((x/2-userNameBlockOffset-blockOutline), (y/2-300), (x/2-userNameBlockOffset+120), (y/2-170)), fill=("#ffffff"))
 draw.rectangle(((x/2-userNameBlockOffset-(blockOutline/2)+75), (y/2-300), (x/2+userNameBlockOffset-(blockOutline/2)-60), (y/2-170)), fill=("#ffffff"))
 draw.ellipse(((x/2+userNameBlockOffset-(blockOutline)-100), (y/2-300), (x/2+userNameBlockOffset-(blockOutline)+20), (y/2-170)), fill=("#ffffff"))
@@ -138,11 +140,12 @@ draw.rectangle(((x/2-statsBlockOffset-(blockOutline/2)+75), (y/2+170), (x/2+stat
 draw.ellipse(((x/2+statsBlockOffset-(blockOutline)-100), (y/2+170), (x/2+statsBlockOffset-(blockOutline)+20), (y/2+300)), fill=("#ffffff"))
 
 textBlocks = textBlocks.filter(ImageFilter.GaussianBlur(8))
-textBlocks.show()
+#textBlocks.show()
 smallBlocks = Image.new(mode="RGBA", size=(1280, 720))
 x, y = smallBlocks.size
 draw = ImageDraw.Draw(smallBlocks)
 
+#these 3 blocks are the main coloured backgrounds
 draw.ellipse(((x/2-userNameBlockOffset+blockOutline), (y/2-285), (x/2-userNameBlockOffset+120), (y/2-185)), fill=("#ffffff"))
 draw.rectangle(((x/2-userNameBlockOffset+75), (y/2-285), (x/2+userNameBlockOffset-60), (y/2-185)), fill=("#ffffff"))
 draw.ellipse(((x/2+userNameBlockOffset-120), (y/2-285), (x/2+userNameBlockOffset-blockOutline), (y/2-185)), fill=("#ffffff"))
@@ -154,12 +157,12 @@ draw.ellipse(((x/2+titleBlockOffset-120), (y/2-50), (x/2+titleBlockOffset-blockO
 draw.ellipse(((x/2-statsBlockOffset+blockOutline), (y/2+185), (x/2-statsBlockOffset+120), (y/2+285)), fill=("#ffffff"))
 draw.rectangle(((x/2-statsBlockOffset+75), (y/2+185), (x/2+statsBlockOffset-60), (y/2+285)), fill=("#ffffff"))
 draw.ellipse(((x/2+statsBlockOffset-120), (y/2+185), (x/2+statsBlockOffset-blockOutline), (y/2+285)), fill=("#ffffff"))
-smallBlocks.show()
+#smallBlocks.show()
 
-textBlocks.show()
+#textBlocks.show()
 textBlocks.convert('RGBA')
 bg.paste(textBlocks, (0, 0), textBlocks)
-bg.show()
+#bg.show()
 
 gradient = Image.new('RGB', imgSize) 
 
@@ -183,13 +186,14 @@ for y in range(imgSize[1]):
         #place the pixel        
         gradient.putpixel((x, y), (int(r), int(g), int(b)))
 
-gradient.show()
+#gradient.show()
 finalImage = Image.new("RGB", imgSize)
 
 #composites all the images so it creates a finished product, not finished yet
 finalImage = Image.composite(gradient, bg, smallBlocks)
-finalImage.show()
+#finalImage.show()
 
+#different font sizes for different blocks to stop unneccesary text/block overflow
 vagRoundUserName = ImageFont.truetype("vag-rounded.ttf", 60)
 vagRoundTitle = ImageFont.truetype("vag-rounded.ttf", 45)
 vagRoundStats = ImageFont.truetype("vag-rounded.ttf", 55)
@@ -199,11 +203,12 @@ if userNameLength < 100:
 else:
     extraOffset = 5
 
+#places text to image
 textDraw = ImageDraw.Draw(finalImage)
 textDraw.text(((x/2 - userNameOffset + extraOffset), 85), userName, fill="#000000", font=vagRoundUserName, stroke_width=2, stroke_fill="#ffffff")
 textDraw.text(((x/2 - titleOffset + extraOffset), 330), finalTitle, fill="#000000", font=vagRoundTitle, stroke_width=2, stroke_fill="#ffffff")
 textDraw.text(((x/2 - statsOffset + extraOffset), 560), stats, fill="#000000", font=vagRoundStats, stroke_width=2, stroke_fill="#ffffff")
 
-finalImage.show()
+#finalImage.show()
 
 input()
